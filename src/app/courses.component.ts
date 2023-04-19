a64d36b70fce9444658e51861e9b439940bd5649
Saqib Aslam <saqib.aslam@awesometechinc.com>
2023-04-20 01:53:51 +0500

    Added services through angular cli. In the component I have used dependency injection

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,8 +1,24 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses',
-    template: '<h2>Discord</h2>'
+    template: `
+    <h4>{{title}}</h4>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{course}}
+        </li>
+    </ul>
+    
+    `
 })
 export class CoursesComponent{
-
-}
+    title = 'This is discord which will help you connect with our community!!'
+    
+    courses; 
+    
+    constructor(services: CoursesService){
+        this.courses = services.getCourses();
+    }
+             
+} 
